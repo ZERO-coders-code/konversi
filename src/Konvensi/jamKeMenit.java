d45d78d3371f5cbd3b7d8d44f9bc1bb41829413b
X240 <X240@DESKTOP-SCEH1CR>
2020-12-19 15:01:51 -0800

    initial commit

---
+++ src/Konvensi/jamKeMenit.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Konvensi;
+
+/**
+ *
+ * @author X240
+ */
+public class jamKeMenit {
+    public static void jamKeMenit(long jam){
+        long menit = jam * 60;
+        System.out.println("menit :"+ menit);
+    }
+    
+}
